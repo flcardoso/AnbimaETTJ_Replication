e7f4d1d2e1d9bb8f2bdfb4f55f7371e2b578cbf9
flcardoso <fernando.cardoso@upf.edu>
2025-11-17 19:21:56 +0100

    work

--- tests/myTesting.R
+++ tests/myTesting.R
@@ -1,5 +1,15 @@
+library("tidyverse")
+
 In <- read.csv("output/ettj_parameters.csv")
 View(In[In$grupo_indexador=="PREFIXADOS",])
 
 In <- read.csv("output/ettj_nominal.csv")
 View(In[In$du==2520,])
+
+In <- read.csv("output/ettj_real.csv")
+View(In)
+
+df <- In %>%
+    select(du, date, rate) %>%
+    tidyr::pivot_wider(names_from = date, values_from = rate)
+View(df)
